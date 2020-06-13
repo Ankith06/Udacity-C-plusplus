2387c1c3f16211e6a1f342072fe60d807aa7d71e
Ankith06 <63002192+Ankith06@users.noreply.github.com>
2020-06-13 13:28:49 +0200

    Add files via upload

---
+++ System Monitor/src/system.cpp
@@ -0,0 +1,82 @@
+#include <unistd.h>
+#include <cstddef>
+#include <set>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+
+
+#include "process.h"
+#include "processor.h"
+#include "system.h"
+
+using std::set;
+using std::size_t;
+using std::string;
+using std::vector;
+
+// TODO: Return the system's CPU
+Processor& System::Cpu() { return cpu_; }
+
+// TODO: Return a container composed of the system's processes
+vector<Process>& System::Processes() { return processes_; }
+
+// TODO: Return the system's kernel identifier (string)
+std::string System::Kernel() {
+  string os, kernel;
+  string line;
+  std::ifstream stream(kProcDirectory + kVersionFilename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> os >> kernel;
+  }
+  return kernel;
+ }
+
+// TODO: Return the system's memory utilization
+float System::MemoryUtilization() { return 0.0; }
+
+// TODO: Return the operating system name
+std::string System::OperatingSystem() { 
+  string line;
+  string key;
+  string value;
+  std::ifstream filestream(kOSPath);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::replace(line.begin(), line.end(), ' ', '_');
+      std::replace(line.begin(), line.end(), '=', ' ');
+      std::replace(line.begin(), line.end(), '"', ' ');
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        if (key == "PRETTY_NAME") {
+          std::replace(value.begin(), value.end(), '_', ' ');
+          return value;
+        }
+      }
+    }
+ }
+
+// TODO: Return the number of processes actively running on the system
+int System::RunningProcesses() { return 0; }
+
+// TODO: Return the total number of processes on the system
+int System::TotalProcesses() { return 0; }
+
+// TODO: Return the number of seconds since the system started running
+long int System::UpTime() { 
+    string up;
+    string idle;
+    string line;
+    std::ifstream stream(kProcDirectory + kUptimeFilename);
+    if (filestream.is_open()) {
+      td::getline(stream, line);
+      std::istringstream linestream(line);
+      linestream >> up >> idle;
+    }
+    float uptime = std::stof(up);
+    return uptime;
+ }
