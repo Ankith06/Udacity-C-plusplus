26a19e6408665b7907997ae8bb86e2e9d8b858e1
Ankith06 <63002192+Ankith06@users.noreply.github.com>
2020-06-18 19:56:19 +0200

    Update process.cpp

--- System Monitor/src/process.cpp
+++ System Monitor/src/process.cpp
@@ -5,28 +5,38 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
+Process::Process(int pid)
+    : pid_(pid){};
+
 // TODO: Return this process's ID
-int Process::Pid() { return 0; }
+int Process::Pid() { return pid_; }
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+float Process::CpuUtilization() { 
+  float totaltime = LinuxParser::ActiveJiffies(Pid());  // in secs
+  float system_uptime = LinuxParser::UpTime(); 
+  float process_uptime = Process::UpTime();
+  float seconds = system_uptime - process_uptime; 
+  return 100 * (totaltime / seconds);
+}
 
 // TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+string Process::Command() { return LinuxParser::Command(Pid()); }
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+string Process::Ram() { return LinuxParser::Ram(Pid()); }
 
 // TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+string Process::User() { return LinuxParser::User(Pid());; }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+long int Process::UpTime() { return LinuxParser::UpTime(Pid());; }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
